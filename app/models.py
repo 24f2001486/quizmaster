e466bf42e1fd57ad99bbd4ba6fa629538fe39044
V RAGHUL KRISHNA <24f2001486@ds.study.iitm.ac.in>
2025-02-05 21:28:41 +0530

    Created database schema for users, subjects, quizzes, and scores. Implemented user registration and admin login functionality.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 
 from app import db
 from datetime import datetime
-    # User model
+
+# User model
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
@@ -10,3 +11,57 @@ class User(db.Model):
     qualification = db.Column(db.String(100), unique=True, nullable=False)
     full_name = db.Column(db.String(100), nullable=False, default=False)
     dob = db.Column(db.DateTime, default = datetime.now)
+
+# Admin model
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+#subject model
+class subject(db.Model):
+    __tablename__ = 'subject'
+    id = db.Column(db.Integer, primary_key=True)
+    subject_name = db.Column(db.String(50), unique=True, nullable=False)
+    subject_description = db.Column(db.String(100), nullable=False)
+
+#chapter model
+class chapter(db.Model):
+    __tablename__ = 'chapter'
+    id = db.Column(db.Integer, primary_key=True)
+    chapter_name = db.Column(db.String(50), unique=True, nullable=False)
+    chapter_description = db.Column(db.String(100), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+
+#quiz model
+class quiz(db.Model):
+    __tablename__ = 'quiz'
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_name = db.Column(db.String(50), unique=True, nullable=False)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+    date_of_quiz = db.Column(db.DateTime, default = datetime.now)
+    Time_duration = db.Column(db.DateTime, nullable=False)
+
+#question model
+class question(db.Model):
+    __tablename__ = 'question'
+    id = db.Column(db.Integer, primary_key=True)
+    question_statement = db.Column(db.String(100), nullable=False)
+    option1 = db.Column(db.String(50), nullable=False)
+    option2 = db.Column(db.String(50), nullable=False)
+    option3 = db.Column(db.String(50), nullable=False)
+    option4 = db.Column(db.String(50), nullable=False)
+    correct_option = db.Column(db.String(50), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+
+#Scores model
+class scores(db.Model):
+    __tablename__ = 'scores'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    total_scored = db.Column(db.Integer, nullable=False)
+    time_stamp_of_attempt = db.Column(db.DateTime, default = datetime.now)
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,15 @@
 
 from datetime import datetime
 from app import app, db
-from app.models import User
+from app.models import Admin, User
 from flask import make_response, redirect, render_template, request, url_for
 
 @app.route('/')
 def index():
+    if Admin.query.filter_by(username='Admin').first() is None:
+        admin =  Admin(username="Admin",password = "Admin")
+        db.session.add(admin)
+        db.session.commit()
     return render_template('index.html')
 
 # user_login.html
@@ -15,6 +19,13 @@ def user_login_page():
         username = request.form['username']
         password = request.form['password']
        
+        if username == 'Admin' and password == 'Admin':
+            resp = make_response(redirect(url_for('admin_dash_page')))
+            resp.set_cookie('username', username)
+            return resp
+        elif username == 'Admin' and password != 'Admin':
+            return render_template('user_login_error.html', message='Invalid username and/or password. Please try again.')
+        
         # Query the database for the user with the given username
         try:
             user = User.query.filter_by(username=username).one()
@@ -27,7 +38,7 @@ def user_login_page():
             resp = make_response(redirect(url_for('user_dash_my_books_page')))
             resp.set_cookie('username', username)
             return resp
-       
+        
         # Redirect to the login page with an error message
         return render_template('user_login_error.html', message='Invalid username and/or password. Please try again.')
    
@@ -49,11 +60,14 @@ def user_register_page():
         if user:
             return render_template('user_register_confirmation.html',message = "Username already exists. Please try with different username.")
         
+        if username == 'Admin':
+            return render_template('user_register_confirmation.html',message = "Username with the name Admin not allowed. Please try with different username.")
+        
         new_user = User(username=username,password = password, full_name = full_name,qualification = qualification,dob = dob)
         db.session.add(new_user)
         db.session.commit()
         return render_template('user_register_confirmation.html', message = "User created successfully. Please login to continue.")
-
+    
 
 
  # Render the register page template for GET requests
